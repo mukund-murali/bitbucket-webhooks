3d19c39f91be69b0e08f598885c31aae626a3b05
Mukund Muralikrishnan <mukund.muralikrishnan@gmail.com>
2020-03-27 21:38:30 +0000

    Correct nulls in json. Add test for pr created

--- tests/test_router.py
+++ tests/test_router.py
@@ -6,18 +6,6 @@ from bitbucket_webhooks_router import decorators
 from bitbucket_webhooks_router import router
 
 
-@decorators.handle_repo_push
-def _repo_push_handler(event):
-    assert event.repository.name == "webhook-test-project"
-    return 1
-
-
-def test_repo_push_router():
-    with open("tests/sample_data/repo_push.json") as f:
-        data = json.load(f)
-    assert router.route("repo:push", data) == [1]
-
-
 def test_no_handler_available():
     assert router.route("random:event", {}) is None
 
@@ -26,5 +14,26 @@ def test_num_handlers():
     assert len(router._HANDLER_MAP) == 7
 
 
-def test_all_different_handlers():
-    pass
+@decorators.handle_repo_push
+def _repo_push_handler(event):
+    assert event.repository.name == "webhook-test-project"
+    return "repo_pushed"
+
+
+def test_repo_push_router():
+    with open("tests/sample_data/repo_push.json") as f:
+        data = json.load(f)
+    assert router.route("repo:push", data) == ["repo_pushed"]
+
+
+@decorators.handle_pr_created
+def _pr_created_handler(event):
+    assert event.pullrequest.title == "PR title"
+    assert event.pullrequest.description == "PR description"
+    return "pr_created"
+
+
+def test_pr_created_router():
+    with open("tests/sample_data/pull_request_created.json") as f:
+        data = json.load(f)
+    assert router.route("pullrequest:created", data) == ["pr_created"]
