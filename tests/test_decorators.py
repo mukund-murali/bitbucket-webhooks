ec1718a8f66646ba358a43d2259d49130db681f9
Mukund Muralikrishnan <mukund.muralikrishnan@gmail.com>
2020-03-27 09:18:39 +0000

    Test for method correctness

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,24 +3,24 @@ from bitbucket_webhooks_router import decorators
 
 @decorators.handle_repo_push
 def sample_repo_push_handler_1(num):
-    return 1
+    return num + 1
 
 
 @decorators.handle_repo_push
 def sample_repo_push_handler_2(num):
-    return 2
+    return num + 2
 
 
 @decorators.handle_pr_approval
 def sample_pr_approval_handler(num):
-    return 3
+    return num + 3
 
 
 def test_handlers():
     assert len(decorators.handle_repo_push.methods) == 2
     results = [method(9) for method in decorators.handle_repo_push.methods]
-    assert results == [1, 2]
+    assert results == [10, 11]
 
     assert len(decorators.handle_pr_approval.methods) == 1
     results = [method(9) for method in decorators.handle_pr_approval.methods]
-    assert results == [3]
+    assert results == [13]
