4231fbf8d519bc0f2610e2ff5fc98be77461f065
Mukund Muralikrishnan <mukund.muralikrishnan@gmail.com>
2020-03-27 11:03:07 +0000

    Changes to publish to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="bb-hooks-router",
-    version="0.0.1",
+    version="0.0.3",
     description="Routes bitbucket webhook API event payloads to easily consumable decorators.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,9 +25,9 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     keywords="bitbucket webhooks development",
-    packages=find_namespace_packages(include=["bitbucket_webhooks_router.*"]),
+    packages=find_namespace_packages(include=["bitbucket_webhooks_router"]),
     python_requires=">=3.5",
-    install_requires=["marshmallow-objects", "marshmallow"],
+    install_requires=["marshmallow-objects>=2.2.2", "marshmallow>=3.0.0"],
     extras_require={"dev": ["pytest", "black", "isort", "flake8", "mypy"]},
     project_urls={
         "Bug Reports": "https://github.com/mukund-murali/bitbucket-webhooks-router/issues",
