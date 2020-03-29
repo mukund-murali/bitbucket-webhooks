ecedd007fb732f2c992e29c5a9f1f7078976b3a7
Mukund Muralikrishnan <mukund.muralikrishnan@gmail.com>
2020-03-29 13:53:59 +0100

    All definitions in one place

--- bitbucket_webhooks/hooks.py
+++ bitbucket_webhooks/hooks.py
@@ -14,9 +14,13 @@ from bitbucket_webhooks import event_schemas
 class _BaseHook:
     """The base hook that creates the decorators."""
 
-    def __init__(self, schema: mo.Model) -> None:
+    _hooks: list = []
+
+    def __init__(self, event_key: str, schema: mo.Model) -> None:
+        self.event_key = event_key
         self.handlers: List[Callable] = []
         self.schema = schema
+        _BaseHook._hooks.append(self)
 
     def __call__(self, method: Callable) -> Callable:
         self.handlers.append(method)
@@ -32,19 +36,29 @@ class _BaseHook:
         event = self.schema().load(event_payload)
         return [method(event) for method in self.handlers]
 
+    @classmethod
+    def get_hooks(cls) -> list:
+        return cls._hooks
 
-repo_push = _BaseHook(event_schemas.RepoPush)
+
+repo_push = _BaseHook("repo:push", event_schemas.RepoPush)
 """
 Decorator for repo:push events with the first argument as
 :class:`bitbucket_webhooks.event_schemas.RepoPush`
 """
 
-pr_approved = _BaseHook(event_schemas.PullRequestApproved)
-pr_unapproved = _BaseHook(event_schemas.PullRequestUnapproved)
-pr_created = _BaseHook(event_schemas.PullRequestCreated)
-pr_updated = _BaseHook(event_schemas.PullRequestUpdated)
-pr_merged = _BaseHook(event_schemas.PullRequestMerged)
-pr_declined = _BaseHook(event_schemas.PullRequestDeclined)
-pr_comment_created = _BaseHook(event_schemas.PullRequestCommentCreated)
-pr_comment_updated = _BaseHook(event_schemas.PullRequestCommentUpdated)
-pr_comment_deleted = _BaseHook(event_schemas.PullRequestCommentDeleted)
+pr_approved = _BaseHook("pullrequest:approved", event_schemas.PullRequestApproved)
+pr_unapproved = _BaseHook("pullrequest:unapproved", event_schemas.PullRequestUnapproved)
+pr_created = _BaseHook("pullrequest:created", event_schemas.PullRequestCreated)
+pr_updated = _BaseHook("pullrequest:updated", event_schemas.PullRequestUpdated)
+pr_merged = _BaseHook("pullrequest:fulfilled", event_schemas.PullRequestMerged)
+pr_declined = _BaseHook("pullrequest:rejected", event_schemas.PullRequestDeclined)
+pr_comment_created = _BaseHook(
+    "pullrequest:comment_created", event_schemas.PullRequestCommentCreated
+)
+pr_comment_updated = _BaseHook(
+    "pullrequest:comment_updated", event_schemas.PullRequestCommentUpdated
+)
+pr_comment_deleted = _BaseHook(
+    "pullrequest:comment_deleted", event_schemas.PullRequestCommentDeleted
+)
--- bitbucket_webhooks/router.py
+++ bitbucket_webhooks/router.py
@@ -2,18 +2,7 @@ from typing import Optional
 
 from bitbucket_webhooks import hooks
 
-_EVENT_HOOK_MAP = {
-    "repo:push": hooks.repo_push,
-    "pullrequest:approved": hooks.pr_approved,
-    "pullrequest:unapproved": hooks.pr_unapproved,
-    "pullrequest:created": hooks.pr_created,
-    "pullrequest:updated": hooks.pr_updated,
-    "pullrequest:fulfilled": hooks.pr_merged,
-    "pullrequest:rejected": hooks.pr_declined,
-    "pullrequest:comment_created": hooks.pr_comment_created,
-    "pullrequest:comment_updated": hooks.pr_comment_updated,
-    "pullrequest:comment_deleted": hooks.pr_comment_deleted,
-}
+_EVENT_HOOK_MAP = {hook.event_key: hook for hook in hooks._BaseHook.get_hooks()}
 
 
 def route(event_key: str, event_payload: dict) -> Optional[list]:
